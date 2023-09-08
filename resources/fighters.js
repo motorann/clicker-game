2114061e05dd06013e64bd3399f507d307218e53
Anna Motorina <scripa57@gmail.com>
2023-09-08 22:50:03 +0300

    Initial commit

---
+++ resources/fighters.js
@@ -0,0 +1,14 @@
+[ 
+    {
+    name: 'first',
+    source: 'https://construct-static.com/uploads/84877/cbbc4a42-4719-4a1a-9f0c-e94ec0fbd13d/zombie-run%20a.gif',
+    }, 
+    {
+    name: 'second',
+    source: 'https://construct-static.com/uploads/84870/dfe6613b-9f19-4030-9533-fc689437c189/zombie-run%20a.gif',
+   }, 
+   {
+  name: 'third',
+  source: 'https://construct-static.com/uploads/84884/c0f61569-c6b7-47f6-93cb-8313d32cc3b7/zombie-walk%20b.gif'
+   }
+]
